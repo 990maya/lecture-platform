541397b201ac9103a4fc8614b714d2879bce3f70
Jounggeun Jo <ddangggoma@gmail.com>
2017-06-11 23:52:05 +0900

    Init Project

---
+++ src/main/java/com/lecture/common/CommonController.java
@@ -0,0 +1,24 @@
+package com.lecture.common;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/")
+public class CommonController {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World");
+        return "welcome";
+    }
+}
